4572719454136af30ee7bfe7ab75295be49aa930
Vishwa Gaurav <vishwagauravin@gmail.com>
2021-12-24 16:00:02 +0530

    at attempt to fix deploy issue

--- components/ToolBox.js
+++ components/ToolBox.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 import Templates from "./Templates";
 
 export default function ToolBox() {
