8ab4300407e448f9b4d54391867ec7403f724fae
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-03 20:44:49 +0530

    added contribute, about us in toolbox

--- components/ToolBox.js
+++ components/ToolBox.js
@@ -168,6 +168,47 @@ export default function ToolBox() {
             <Templates onClose={() => setIsTemplateVisible(false)} />
           )}
         </div>
+        {/* Contribute */}
+        <a
+          className="inline-flex flex-col justify-center items-center ml-2 w-24 cursor-pointer"
+          href="https://github.com/VishwaGauravIn/pro-writer"
+          target="_blank"
+          rel="noopener"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="-0.5 -1 17 17"
+            stroke="currentColor"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+          Contribute
+        </a>
+        {/* About */}
+        <a
+          className="inline-flex flex-col justify-center items-center ml-2 w-24 cursor-pointer"
+          href="https://itsvg.in"
+          target="_blank"
+          rel="noopener"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          About Us
+        </a>
       </div>
     </>
   );
